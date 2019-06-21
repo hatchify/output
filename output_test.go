b4c8c43629e92d2157bd21b79e425e2a21d34d81
Josh Montoya <josh@montoya.io>
2019-06-21 12:30:19 -0700

    Add TestAll

--- output_test.go
+++ output_test.go
@@ -1,5 +1,15 @@
 package output
 
+import "testing"
+
+func TestAll(t *testing.T) {
+	Print("This is an example basic message")
+	Success("This is an example success message")
+	Warning("This is an example warning message")
+	Error("This is an example error message")
+	Debug("This is an example debug message")
+}
+
 func ExamplePrint() {
 	Print("Hello world!")
 }
