c0e43694d53d682d74aa1b0bd4bb574dc24ac136
Josh Montoya <josh@montoya.io>
2019-06-21 13:46:26 -0700

    Implement wrapper

--- utils.go
+++ utils.go
@@ -12,10 +12,21 @@ func getDebugVals() (filename string, lineNumber int) {
 	return
 }
 
-// PrefixFunc is used to set a prefix to a log line
-type PrefixFunc func() string
-
 // SetRFC3339Timestamp is a helper function for setting RFC3339 timestamp
 func SetRFC3339Timestamp() string {
 	return time.Now().Format(time.RFC3339) + " :: "
+}
+
+// PrefixFunc is used to set a prefix to a log line
+type PrefixFunc func() string
+
+// Outputter represents an outputter interface
+type Outputter interface {
+	Print(msg string)
+	Printf(format string, values ...interface{})
+	Notification(format string, values ...interface{})
+	Success(format string, values ...interface{})
+	Warning(format string, values ...interface{})
+	Error(format string, values ...interface{})
+	Debug(format string, values ...interface{})
 }
